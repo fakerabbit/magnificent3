98cad14e32b9605a4f253a9556f55a0f8df1fa9d
Mirko <mirkoj@gmail.com>
2015-08-20 18:55:05 -0500

    Implemented tiled background

--- MagnificentThree/MenuView.swift
+++ MagnificentThree/MenuView.swift
@@ -20,6 +20,7 @@ class MenuView: SKView {
     let pad: CGFloat = 30.0
     let tpad: CGFloat = 10.0
     
+    private var cscene: SKScene?
     private var logoIv: UIImageView?
     private var townIv: UIImageView?
     private var town: SKSpriteNode?
@@ -30,21 +31,23 @@ class MenuView: SKView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.backgroundColor = UIColor(patternImage: UIImage(named: "BgTile")!)
+        //self.backgroundColor = UIColor(patternImage: UIImage(named: "BgTile")!)
         
         logoIv = UIImageView(image: UIImage(named: "Logo"))
         logoIv?.contentMode = UIViewContentMode.ScaleAspectFit
         self.addSubview(logoIv!)
         
-        var scene: SKScene = SKScene(size: self.bounds.size)
-        scene.backgroundColor = UIColor.clearColor()
+        cscene = SKScene(size: self.bounds.size)
+        //scene.backgroundColor = UIColor.clearColor()
         //scene.scaleMode = .SKSceneScaleModeAspectFill
+        
+        self.fillBackground()
         
         townIv = UIImageView(image: UIImage(named: "BgTown"))
         townIv?.contentMode = UIViewContentMode.ScaleToFill
         
         town = SKSpriteNode(imageNamed: "BgTown")
-        scene.addChild(town!)
+        cscene?.addChild(town!)
         
         
         var path = NSBundle.mainBundle().pathForResource("pfHay", ofType: "sks")
@@ -53,9 +56,9 @@ class MenuView: SKView {
         hayParticle!.zPosition = 100
         hayParticle!.name = "Hay"
         hayParticle!.targetNode = scene
-        scene.addChild(hayParticle!)
+        cscene?.addChild(hayParticle!)
         
-        self.presentScene(scene)
+        self.presentScene(cscene)
         
         let seq = SKAction.sequence([theme, SKAction.waitForDuration(30.0)])
         let repeatAction = SKAction.repeatActionForever(seq)
@@ -78,8 +81,39 @@ class MenuView: SKView {
             return
         }
         
-        logoIv?.frame = CGRectMake(pad, 20, w - pad * 2, logoIv!.frame.size.height)
+        logoIv?.frame = CGRectMake(pad, 0, w - pad * 2, logoIv!.frame.size.height)
         townIv?.frame = CGRectMake(-tpad, h - townIv!.frame.size.height + tpad, w + tpad * 2, townIv!.frame.size.height)
         town?.position = CGPointMake(w/2, townIv!.frame.size.height/2)
+    }
+    
+    // MARK: Private methods
+    
+    private func fillBackground() {
+        
+        let tile = SKTexture(imageNamed: "BgTile")
+        var totH: CGFloat = 0
+        var totW: CGFloat = 0
+        var i: Int = 0
+        var j: Int = 0
+        
+        while totH < UIScreen.mainScreen().bounds.size.height + tile.size().height {
+            
+            if totW >= UIScreen.mainScreen().bounds.size.width {
+                totW = 0
+                i = 0
+            }
+            
+            while totW < UIScreen.mainScreen().bounds.size.width + tile.size().width {
+                let bg = SKSpriteNode(texture: tile)
+                bg.zPosition = -100
+                bg.position = CGPointMake(CGFloat(i) * tile.size().width, CGFloat(j) * tile.size().height)
+                cscene?.addChild(bg)
+                i++
+                totW += tile.size().width
+            }
+            
+            j++
+            totH += tile.size().height
+        }
     }
 }
